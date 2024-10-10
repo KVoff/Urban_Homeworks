b13ad05044b15a7ed168c7c6f929332a71c4dc0c
Vladimir Kolesnikov <vk.coworking@gmail.com>
2024-10-10 18:42:09 +0300

    Add homework_17_2

---
+++ module_17_bak/src/module_17/lesson_17/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from routers import category
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def welcome():
+    return {"message": "My Shop"}
+
+
+app.include_router(category.router)
